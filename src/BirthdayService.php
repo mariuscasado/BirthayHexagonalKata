e3e0b2a6fb5f9528b610da8862897d831ad71294
Màrius Casadó <mariuscasado@gmail.com>
2023-07-10 11:32:29 +0200

    extract obtain employee to class

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -12,13 +12,16 @@ use Swift_SmtpTransport;
 
 class BirthdayService
 {
+    private CsvEmployeeRepository $repository;
+
     public function sendGreetings(
         string $fileName,
         OurDate $ourDate,
         string $smtpHost,
         int $smtpPort
     ): void {
-        $employees = $this->getEmployees($fileName);
+        $this->repository = new CsvEmployeeRepository($fileName);
+        $employees = $this->repository->getEmployees();
 
         foreach($employees as $employee) {
             if ($employee->isBirthday($ourDate)) {
@@ -54,18 +57,4 @@ class BirthdayService
     {
         $mailer->send($msg);
     }
-
-    private function getEmployees(string $fileName): array
-    {
-        $fileHandler = fopen($fileName, 'rb');
-        fgetcsv($fileHandler);
-        $employees = [];
-        while ($employeeData = fgetcsv($fileHandler, null)) {
-            $employeeData = array_map('trim', $employeeData);
-            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
-            $employees[] = $employee;
-        }
-        return $employees;
-    }
-
 }
---
+++ src/CsvEmployeeRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class CsvEmployeeRepository
+{
+    private string $fileName;
+
+    public function __construct(string $fileName)
+    {
+        $this->fileName = $fileName;
+    }
+
+    /**
+     * @return Employee[]
+     */
+    public function getEmployees(): array
+    {
+        $fileHandler = fopen($this->fileName, 'rb');
+        fgetcsv($fileHandler);
+        $employees = [];
+        while ($employeeData = fgetcsv($fileHandler, null)) {
+            $employeeData = array_map('trim', $employeeData);
+            $employee = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+            $employees[] = $employee;
+        }
+        return $employees;
+    }
+}
